48ad095f07d663da678ebae0df3dd63c9e5df089
gaeun <knospe1@naver.com>
2022-08-19 14:28:06 +0900

    feat: solve 'Bob'

---
+++ bob/lib/bob.dart
@@ -0,0 +1,18 @@
+class Bob {
+  String response(String question) {
+    question = _removeSpaceAndDigit(question);
+
+    if (_isNoWord(question)) return 'Fine. Be that way!';
+    if (_isQuestion(question) && _isYelling(question)) return "Calm down, I know what I'm doing!";
+    if (_isQuestion(question)) return 'Sure.';
+    if (_isYelling(question)) return 'Whoa, chill out!';
+
+    return 'Whatever.';
+  }
+
+  bool _isQuestion(String question) => question.endsWith('?');
+  bool _isYelling(String question) => !question.contains(RegExp('[a-z]')) && question.contains(RegExp('[A-Z]'));
+  bool _isNoWord(String question) => !question.contains(RegExp('[a-z]||[A-Z]')) || question.isEmpty;
+  String _removeSpaceAndDigit(String question) => question.trim().replaceAll(RegExp('[0-9]'), '');
+}
+
