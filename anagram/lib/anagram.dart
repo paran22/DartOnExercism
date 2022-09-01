63dc349205e3aad1cf3ce534076a0fe21adeef44
gaeun <knospe1@naver.com>
2022-09-01 10:32:30 +0900

    refactor: refactor 'Anagram' solution

--- anagram/lib/anagram.dart
+++ anagram/lib/anagram.dart
@@ -1,19 +1,15 @@
 class Anagram {
-  List<String> findAnagrams(String word, List<String> list) {
-    List<String> answer = [];
-
-    for (String element in list) {
-      if (word.length != element.length) continue;
-      if (word.toLowerCase() == element.toLowerCase()) continue;
-
-      List<String> words = word.toLowerCase().split('');
-      for (int i = 0; i < element.length; i++) {
-        if (words.contains(element[i].toLowerCase())) words.remove(element[i].toLowerCase());
-      }
-      if (words.length == 0 ) answer.add(element);
-    }
-
-    return answer;
+  String makeKey(String word) {
+    List<String> words = word.toLowerCase().split('');
+    words.sort();
+    return words.toString();
   }
 
+  List<String> findAnagrams(String word, List<String> list) {
+    return list
+        .where((element) =>
+            (makeKey(word) == makeKey(element)) &&
+            (element.toLowerCase() != word.toLowerCase()))
+        .toList();
+  }
 }
