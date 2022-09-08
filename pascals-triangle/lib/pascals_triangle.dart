a2b233c1c666280324c182fac4645a47c0e75734
gaeun <knospe1@naver.com>
2022-09-08 15:37:08 +0900

    feat: solve 'Pascals Triangle'

---
+++ pascals-triangle/lib/pascals_triangle.dart
@@ -0,0 +1,35 @@
+class PascalsTriangle {
+  List<List<int>> rows(int count) {
+    List<List<int>> answer = [];
+
+    for (int i = 0; i < count; i++) {
+      if (i == 0) {
+        answer.add([1]);
+        continue;
+      }
+      if (i == 1) {
+        answer.add([1, 1]);
+        continue;
+      }
+      answer.add(createRow(answer[i - 1], i));
+    }
+    return answer;
+  }
+
+  List<int> createRow(List<int> before, int count) {
+    List<int> row = [];
+    for (int i = 0; i < count + 1; i++) {
+      if (i == 0) {
+        row.add(1);
+        continue;
+      }
+      if (i == count) {
+        row.add(1);
+        continue;
+      }
+      row.add(before[i - 1] + before[i]);
+    }
+
+    return row;
+  }
+}
